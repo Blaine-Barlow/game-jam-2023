20ae29d86b0a164566a24d3af830b813f9ca46c2
Blaine Barlow <barlow.blaine1@gmail.com>
2023-01-07 15:08:10 -0600

    Added camera boundaries and player location parameters

---
+++ game/Assets/Scripts/LeftWall.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftWall : MonoBehaviour
+{
+    [SerializeField]
+    private float moveLocation;
+    
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            Debug.Log("Hit left Wall");
+            other.gameObject.transform.position = new Vector3(moveLocation, other.gameObject.transform.position.y, other.gameObject.transform.position.z);
+        }
+    }
+}
